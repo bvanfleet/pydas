73c89584d52d77e76bd77d69a6c403ccb96f1a18
Bradley Van Fleet <bradley.vanfleet@healthcatalyst.com>
2021-02-22 17:08:03 -0700

    Fix unit test fixture.

--- tests/pydas/fixtures/client.py
+++ tests/pydas/fixtures/client.py
@@ -1,7 +1,8 @@
-from pydas import app
+from pydas import create_app
 
 
 def app_client():
+    app = create_app()
     app.config["TESTING"] = True
     app.config["DB_DIALECT"] = 'mock'
     return app.test_client()
